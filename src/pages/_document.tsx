342802474cbd463066e8fcf4179dbd8701f148c7
Naufal Rafi <naufalrafiar@gmail.com>
2024-11-23 13:19:27 +1000

    misc: add g-ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7766460411836759"
+        crossOrigin="anonymous"
+      ></script>
       <body className="">
         <Main />
         <NextScript />
